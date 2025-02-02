ce6601d4f4ee647ae8637f80aa28444c1f677836
Marco Pivetta <ocramius@gmail.com>
2025-02-02 13:20:54 +0100

    Basic testing of `IssueKey` details

--- src/Jira/Domain/IssueKey.php
+++ src/Jira/Domain/IssueKey.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace TimeSync\Jira\Domain;
 
+use Psl;
+use Psl\Exception\InvariantViolationException;
+use Psl\Regex;
+use function sprintf;
+
 /** @psalm-immutable */
 final readonly class IssueKey
 {
@@ -11,5 +16,20 @@ final readonly class IssueKey
     private function __construct(
         public string $key,
     ) {
+    }
+
+    /**
+     * @pure 
+     * @param non-empty-string $key 
+     * @throws InvariantViolationException
+     */
+    public static function make(string $key): self
+    {
+        Psl\invariant(
+            Regex\matches($key, '/^[A-Z0-9]+-[1-9]+\d*$/'),
+            sprintf('Issue "%s" does not look like a jira ID', $key),
+        );
+        
+        return new self($key);
     }
 }
